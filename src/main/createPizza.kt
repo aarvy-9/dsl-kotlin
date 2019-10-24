03c9c16ad8e7ec2fff303504d8d89d2a7fca5d50
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-10-24 03:25:09 +0000

    Initial checkin with errors
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign3/Kotlin@2411 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/main/createPizza.kt
+++ src/main/createPizza.kt
@@ -1,5 +1,14 @@
-infix fun process(block: () -> Unit) {
-    println(block)
+class Pizza {       
+	val Pizza = this	
+	infix fun create(item: String){
+		Pizza = item
+	}
 }
 
+
+object process{
+	infix fun dsl(block: Pizza.() -> Unit) : Pizza{
+		return Pizza().apply(block)
+	}
+}
 
--- src/test/testCreatePizza.kt
+++ src/test/testCreatePizza.kt
@@ -2,15 +2,14 @@ import kotlin.test.assertEquals
 import org.junit.Test
 
 
-
 class TestSource {
     @Test fun testCanary() {
 		assertEquals(true, true)
     }
 	
 	@Test fun testCreatePizza() {
-		dslString = "process".dsl {}
-		assertEquals(dslString, true)
+		val dslString = process dsl {}
+		assertEquals(dslString, "")
 	}
 	
 	
