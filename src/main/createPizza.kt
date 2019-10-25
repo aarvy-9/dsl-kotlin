b46b43c914bcfd94b21f7dce1883fad352c30015
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-10-25 22:44:17 +0000

    Kotlin code modified
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign3/Kotlin@2497 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/main/createPizza.kt
+++ src/main/createPizza.kt
@@ -1,13 +1,45 @@
 class Pizza {
-  override fun toString() = "Pizza:"
+	val pizza = this
+	val add = this
+	val start = this
+	val ingredients: MutableList<String> = mutableListOf()
+	
+	infix fun spread(input1: String) {
+	
+		ingredients.add(input1)
+		
+	}
+	
+	infix fun topping(input: String) {
+		
+		ingredients.add(input)
+		
+	}
+	
+	infix fun bake(input: String) {
+		
+		ingredients.add("baked")
+		
+	}
+	
+	override fun toString() = "Pizza: ${ingredients.joinToString()}".trim()
+	
 }
 
 object create {
-  infix fun Pizza(block: () -> Unit): String {
+
+  infix fun Pizza(block: Pizza.() -> Unit): String {
     val toBuild = Pizza()
     
-    block()
+    toBuild.block()
     
     return toBuild.toString()
   }
+  
+}
+
+fun Pizza.toppings(block: Pizza.() -> Unit) {
+
+	block()
+	
 }
--- src/test/testCreatePizza.kt
+++ src/test/testCreatePizza.kt
@@ -2,13 +2,63 @@ import kotlin.test.assertEquals
 import org.junit.Test
 
 class TestSource {
-  @Test fun testCanary() {
+	@Test fun testCanary() {
 		assertEquals(true, true)
-  }
+	}
 	
 	@Test fun testCreatePizza() {
 		val result = create Pizza {}
  
- 	  assertEquals("Pizza:", result)
+		assertEquals("Pizza:", result)
+	}
+	
+	@Test fun testSpreadCheese() {
+		val result = create Pizza {
+			add spread "cheese"
+		}
+
+		assertEquals("Pizza: cheese", result)
+	}
+	
+	@Test fun testSpreadToppings() {
+		val result = create Pizza{
+			add spread "cheese"
+			toppings {
+				add topping "onions"
+				add topping "jalapenos"
+				add topping "green_pepper"
+			}
+		}
+		
+		assertEquals("Pizza: cheese, onions, jalapenos, green_pepper", result)
+	}
+	
+	@Test fun testSpreadSauce() {
+		val result = create Pizza{
+			add spread "cheese"
+			toppings {
+				add topping "onions"
+				add topping "jalapenos"
+				add topping "green_pepper"
+			}
+			add spread "sauce"
+		}
+		
+		assertEquals("Pizza: cheese, onions, jalapenos, green_pepper, sauce", result)
+	}
+	
+	@Test fun testBaked() {
+		val result = create Pizza{
+			add spread "cheese"
+			toppings {
+				add topping "onions"
+				add topping "jalapenos"
+				add topping "green_pepper"
+			}
+			add spread "sauce"
+			start bake ""
+		}
+		
+		assertEquals("Pizza: cheese, onions, jalapenos, green_pepper, sauce, baked", result)
 	}
 }
